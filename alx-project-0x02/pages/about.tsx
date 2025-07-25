53fa88abe2bffd1cf53ddd1d4f5e272b2ade9ae1
Alle4n <allennajjuma96@gmail.com>
2025-07-25 16:24:27 +0300

    Basic Routing in Next.js

---
+++ alx-project-0x02/pages/about.tsx
@@ -0,0 +1,7 @@
+const About = () => (
+  <div className="p-6">
+    <h1 className="text-2xl">About Page</h1>
+  </div>
+);
+
+export default About;
---
+++ alx-project-0x02/pages/home.tsx
@@ -0,0 +1,7 @@
+const Home = () => (
+  <div className="p-6">
+    <h1 className="text-2xl">Home Page</h1>
+  </div>
+);
+
+export default Home;
