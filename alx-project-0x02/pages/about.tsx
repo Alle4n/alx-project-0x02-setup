74a3d2bad53f1c4960c5fdfedeacc77ea518017a
Alle4n <allennajjuma96@gmail.com>
2025-07-25 17:28:07 +0300

    Create and Use a Button Component

--- alx-project-0x02/pages/about.tsx
+++ alx-project-0x02/pages/about.tsx
@@ -1,8 +1,7 @@
-import Button from "@/components/common/button";
+import Button from "@/components/common/Button";
 
 const About = () => (
   <div className="p-6 space-x-4">
-    <h1 className="text-2xl">About Page</h1>
     <Button title="Small" size="small" shape="rounded-sm" />
     <Button title="Medium" size="medium" shape="rounded-md" />
     <Button title="Large" size="large" shape="rounded-full" />
