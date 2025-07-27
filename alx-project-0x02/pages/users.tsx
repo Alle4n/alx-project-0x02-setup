c9b2ccd58e4693062702f0ec56ef7220df3da5c7
Alle4n <allennajjuma96@gmail.com>
2025-07-27 07:42:29 +0300

    Build a UserCard Component

--- alx-project-0x02/pages/users.tsx
+++ alx-project-0x02/pages/users.tsx
@@ -3,32 +3,28 @@ import Header from "@/components/layout/Header";
 import UserCard from "@/components/common/UserCard";
 import { UserProps } from "@/interfaces";
 
-interface UsersPageProps {
-  users: UserProps[];
-}
-
-const Users: React.FC<UsersPageProps> = ({ users }) => {
-  return (
-    <div className="min-h-screen bg-gray-100">
-      <Header />
-      <div className="max-w-4xl mx-auto p-6 grid gap-4">
+const Users: React.FC<{ users: UserProps[] }> = ({ users }) => (
+  <div>
+    <Header />
+    <main className="container mx-auto p-6">
+      <h1 className="text-3xl font-bold mb-6">Users</h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {users.map((user) => (
           <UserCard key={user.id} {...user} />
         ))}
       </div>
-    </div>
-  );
-};
+    </main>
+  </div>
+);
 
-export const getStaticProps: GetStaticProps = async () => {
+export async function getStaticProps() {
   const res = await fetch("https://jsonplaceholder.typicode.com/users");
   const users = await res.json();
 
   return {
-    props: {
-      users,
-    },
+    props: { users },
+    revalidate: 60,
   };
-};
+}
 
 export default Users;
